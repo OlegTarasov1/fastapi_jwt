a670d9aed8c3118d33db0aabbf6d8d937aed2cde
Oleg Tarasov <oldoleg6@mail.ru>
2025-01-17 12:50:21 +0300

    minor fixes

--- jwt_app/user_book_manip/user_book_apis.py
+++ jwt_app/user_book_manip/user_book_apis.py
@@ -4,7 +4,9 @@ from user_book_manip.user_book_requests import AsyncRequests
 
 user_book_router = APIRouter()
 
-user_book_router.post('/api/v1/assign_book/{id}/{book_id}', tags = ['user_book'])
+@user_book_router.post('/api/v1/assign_book/{id}/{book_id}', tags = ['user_book'])
 async def add_book_to_user(id: int, book_id: int, token: dict = Depends(is_creator_or_admin)):
     resp = await AsyncRequests.add_book(id, book_id) 
     return resp
+
+
